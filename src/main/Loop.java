25f14148ff35f7eb731dbefaea72b0bef1b126a3
bbuck108 <bbuck108@gmail.com>
2016-02-16 11:40:25 -0500

    Add main classes

---
+++ src/main/Loop.java
@@ -0,0 +1,9 @@
+package main;
+
+public class Loop {
+	public static void loop(){
+		while(true){
+			System.out.println("Loop");
+		}
+	}
+}
---
+++ src/main/Start.java
@@ -0,0 +1,8 @@
+package main;
+
+public class Start {
+	public static void main(String[] args){
+		System.out.println("Start");
+		Loop.loop();
+	}
+}
