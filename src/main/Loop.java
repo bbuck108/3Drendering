27796648c0fa284ff0ae7967d7bc03ab27487a50
bbuck108 <bbuck108@gmail.com>
2016-02-16 12:20:54 -0500

    Add more LWJGL Update buildpath Setup basic window

--- src/main/Loop.java
+++ src/main/Loop.java
@@ -1,9 +1,16 @@
 package main;
 
+import org.lwjgl.opengl.Display;
+
 public class Loop {
 	public static void loop(){
-		while(true){
-			System.out.println("Loop");
-		}
+		while (!Display.isCloseRequested()) {
+            
+            // render OpenGL here
+             
+            Display.update();
+        }
+         
+        Display.destroy();
 	}
 }
--- src/main/Start.java
+++ src/main/Start.java
@@ -1,8 +1,26 @@
 package main;
 
+import org.lwjgl.LWJGLException;
+import org.lwjgl.opengl.Display;
+import org.lwjgl.opengl.DisplayMode;
+import org.lwjgl.opengl.GL11;
+
 public class Start {
 	public static void main(String[] args){
-		System.out.println("Start");
+		try{
+			Display.setDisplayMode(new DisplayMode(800,600));
+			Display.setFullscreen(true);
+			Display.create();
+		} catch (LWJGLException e){
+			e.printStackTrace();
+			System.exit(0);
+		}
+		
+		GL11.glMatrixMode(GL11.GL_PROJECTION);
+		GL11.glLoadIdentity();
+		GL11.glOrtho(0, 800, 0, 600, 1, -1);
+		GL11.glMatrixMode(GL11.GL_MODELVIEW);
+		
 		Loop.loop();
 	}
 }
