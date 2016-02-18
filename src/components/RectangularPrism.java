4abd53afd8950ee902327342cf9c5d3f7985a271
bbuck108 <bbuck108@gmail.com>
2016-02-18 10:25:36 -0500

    Add methods to find surface area and volume

--- src/components/RectangularPrism.java
+++ src/components/RectangularPrism.java
@@ -15,4 +15,18 @@ public class RectangularPrism {
 		motion = p_3;
 		color = p_4;
 	}
+	public double findVolume(){
+		double t_x = Math.abs(v1.getX()-v2.getX());
+		double t_y = Math.abs(v1.getY()-v2.getY());
+		double t_z = Math.abs(v1.getZ()-v2.getZ());
+		
+		return (t_x * t_y * t_z);
+	}
+	public double findSurfaceArea(){
+		double t_x = Math.abs(v1.getX()-v2.getX());
+		double t_y = Math.abs(v1.getY()-v2.getY());
+		double t_z = Math.abs(v1.getZ()-v2.getZ());
+		
+		return (2*t_x*t_y + 2*t_x*t_z + 2*t_y*t_z);
+	}
 }
