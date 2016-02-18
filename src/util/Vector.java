e47e0aa20599ca0bc7af6055805b4989f1666677
18connoralehmacher <18connoralehmacher@HDHSID033303.sdpc.local>
2016-02-18 10:21:55 -0500

    Improve Vector some more

--- src/util/Vector.java
+++ src/util/Vector.java
@@ -3,9 +3,9 @@ package util;
 public class Vector {
 	//----------Fields----------//
 	// Basic Information   
-	public double x;
-	public double y;
-	public double z;
+	private double x;
+	private double y;
+	private double z;
 	
 	//------------Constructors------------//
 	private Vector(double x, double y, double z) {
@@ -17,16 +17,22 @@ public class Vector {
 	/** Default; 0 */
 	public Vector() {}
 	
-	//-----------Methods------------//
+	//-----------Puesdo-Constructors------------//
 	public static Vector createFromRectangular(double x, double y, double z) {
 		return new Vector(x, y, z);
 	}
 	
-	public static Vector createFromCylindrical() {
+	public static Vector createFromCylindrical(double r, double theta, double z) {
+		
 		return new Vector();
 	}
 	
 	public static Vector createFromSpherical() {
 		return new Vector();
 	}
+	
+	//------------Getter-Methods-----//
+	public double getX() {return x;}
+	public double getY() {return y;}
+	public double getZ() {return z;}
 }
