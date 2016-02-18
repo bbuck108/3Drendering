96a6270b26fe91d372c85c82ec65b183e93a53ed
bbuck108 <bbuck108@gmail.com>
2016-02-18 10:31:56 -0500

    Improve javadoc

--- src/components/RectangularPrism.java
+++ src/components/RectangularPrism.java
@@ -6,23 +6,36 @@ public class RectangularPrism {
 	Vector v1;
 	Vector v2;
 	Vector motion;
+	double mass;
 	
 	Color color;
 	
-	public RectangularPrism(Vector p_1, Vector p_2, Vector p_3, Color p_4){
+	/**
+	 * Constructs a rectangular prism based on two points
+	 * @param p_1 One corner of the prism
+	 * @param p_2 A corner completely opposite of the first
+	 * @param p_3 A vector specifying the object's initial velocity
+	 * @param p_4 Mass of the prism
+	 * @param p_5 The color of the rectangle
+	 */
+	public RectangularPrism(Vector p_1, Vector p_2, Vector p_3, double p_4,Color p_5){
 		v1 = p_1;
 		v2 = p_2;
 		motion = p_3;
-		color = p_4;
+		mass = p_4;
+		color = p_5;
 	}
-	public double findVolume(){
+	
+	/** Returns the volume of the prism*/
+	public double volume(){
 		double t_x = Math.abs(v1.getX()-v2.getX());
 		double t_y = Math.abs(v1.getY()-v2.getY());
 		double t_z = Math.abs(v1.getZ()-v2.getZ());
 		
 		return (t_x * t_y * t_z);
 	}
-	public double findSurfaceArea(){
+	/** Returns the surface area of the prism*/
+	public double surfaceArea(){
 		double t_x = Math.abs(v1.getX()-v2.getX());
 		double t_y = Math.abs(v1.getY()-v2.getY());
 		double t_z = Math.abs(v1.getZ()-v2.getZ());
