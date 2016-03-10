e3bf31958870e71be3b2454d74771a3aa8a6ecec
Connor Lehmacher <clehmac@hotmail.com>
2016-03-10 10:17:44 -0500

    Make PhysicalObject much better

--- src/components/Sphere.java
+++ src/components/Sphere.java
@@ -1,18 +1,43 @@
 package components;
 
+import java.awt.Color;
+
+import main.Start;
 import util.Util;
 
-public class Sphere extends Shape{
+public class Sphere extends PhysicalObject{
+	Vector center;
 	double radius;
 	
-	public Sphere(Vector c, double r){
-		super(c);
+	public Sphere(Vector c, double r, Vector v, double m, Color color){
+		center = c;
 		radius = r;
+		motion = v;
+		mass = m;
+		this.color = color;
+		Start.addToRenderList(this);
+	}
+	
+	public Sphere(Vector c, double r, Vector v, double m, Color color, boolean list){
+		center = c;
+		radius = r;
+		motion = v;
+		mass = m;
+		this.color = color;
+		if(list){Start.addToRenderList(this);}
 	}
 	
 	@Override
+	public Sphere translateBy(Vector p) {
+		return new Sphere(center.plus(p), radius, motion, mass, color, false);
+	}
+	
+	public Vector getSurfaceNormal(Vector p) {
+		return (new Segment(center, p)).direction();
+	}
+	
 	public double isIntersecting(Ray ray) {
-		Vector EO = (new Segment(ray.origin, location)).direction();
+		Vector EO = (new Segment(ray.origin, center)).direction();
 		Vector V  = Vector.createFromSpherical(EO.norm(), ray.theta, ray.phi);
 		double v = EO.dot(V);
 		double disc = Util.sq(radius) - (Util.sq(EO.norm())-v);
@@ -28,12 +53,8 @@ public class Sphere extends Shape{
 	}
 	
 	@Override
-	public Vector getSurfaceNormal(Vector p) {
-		return (new Segment(location, p)).direction();
+	public void editTo(PhysicalObject s) {
+		center = ((Sphere)s).center;
+		radius = ((Sphere)s).radius;
 	}
-	
-	@Override
-	public Sphere translateBy(Vector p) {
-		return new Sphere(location.plus(p), radius);
-	}	
 }
