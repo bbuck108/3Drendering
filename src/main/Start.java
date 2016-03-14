47a0e4a8f773ad40615b236a7109bc781e000244
Connor Lehmacher <clehmac@hotmail.com>
2016-03-14 10:16:28 -0400

    Rework Main a bit

--- src/main/Start.java
+++ src/main/Start.java
@@ -12,16 +12,17 @@ import components.Camera;
 import components.PhysicalObject;
 import components.RectangularPrism;
 import components.Rotation;
-import components.Sphere;
 import components.Vector;
 
 public class Start {
 	public static ArrayList<PhysicalObject> renderList;
 	public static Camera camera;
 	static RectangularPrism x;
+	
 	public static void addToRenderList(PhysicalObject p_1) {
 		renderList.add(p_1);
 	}
+	
 	public static void main(String[] args) {
 		renderList = new ArrayList<PhysicalObject>();
 		
@@ -29,9 +30,16 @@ public class Start {
 		camera = new Camera(Vector.createFromRectangular(0, 0, 0), Vector.createFromRectangular(800, 600, 600), Vector.createFromRectangular(0, 0, 0));
 		//
 		//addToRenderList(new PhysicalObject(Vector.createFromRectangular(0, 0, 50), 1, Color.YELLOW, new Sphere(Vector.createFromRectangular(0, 0, 1000), 100)));
-		addToRenderList(new PhysicalObject(Vector.createFromRectangular(0, 0, 0), 1, Color.RED, new RectangularPrism(Vector.createFromRectangular(0, 0, 1000), Vector.createFromRectangular(100, 100, 100), new Rotation(0, 0, 0))));
+		addToRenderList(new PhysicalObject(Vector.createFromRectangular(0, 0, 0),
+				1, Color.RED, 
+				new RectangularPrism(Vector.createFromRectangular(0, 0, 1000),
+						Vector.createFromRectangular(100, 100, 100),
+						new Rotation(0, 0, 0))
+				));
 		//
 		
+		
+		//Builds the screen
 		try{
 			Display.setDisplayMode(new DisplayMode(800,600));
 			Display.setFullscreen(true);
@@ -41,11 +49,37 @@ public class Start {
 			System.exit(0);
 		}
 		
+		
+		//I have no idea what this does
 		GL11.glMatrixMode(GL11.GL_PROJECTION);
 		GL11.glLoadIdentity();
 		GL11.glOrtho(0, 800, 0, 600, 1, -1);
 		GL11.glMatrixMode(GL11.GL_MODELVIEW);
 		
-		Loop.loop();
+		//Begins the game loop
+		loop();
+	}
+	
+	public static void loop() {
+		while (!Display.isCloseRequested()) {
+			//Move
+			for(PhysicalObject t_1: Start.renderList){
+				t_1.move();
+			}
+			
+			
+			// I have no idea what this does...
+			// When this is causing a problem I'm sorry
+			GL11.glClear(GL11.GL_COLOR_BUFFER_BIT | GL11.GL_DEPTH_BUFFER_BIT); 
+		
+            // render OpenGL here
+			Start.camera.render();
+			System.out.println("Done rendering");
+             
+			//Update display
+            Display.update();
+        }
+         
+        Display.destroy();
 	}
 }
