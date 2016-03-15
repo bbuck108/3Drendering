7fed3514429700061356ac8bdad2dd11d2aaf492
bbuck108 <bbuck108@gmail.com>
2016-03-14 22:06:56 -0400

    Add some javadoc

--- src/main/Start.java
+++ src/main/Start.java
@@ -1,6 +1,5 @@
 package main;
 
-import java.awt.Color;
 import java.io.File;
 import java.util.ArrayList;
 
@@ -15,7 +14,6 @@ import org.lwjgl.opengl.GL11;
 import components.Camera;
 import components.PhysicalObject;
 import components.RectangularPrism;
-import components.Sphere;
 import components.Vector;
 import file.ParseFile;
 
@@ -29,22 +27,56 @@ public class Start {
 		renderList.add(p_1);
 	}
 	
-	public static void main(String[] args) {
-		renderList = new ArrayList<PhysicalObject>();
-		
+	/**
+	 * Opens a File Chooser dialog to select a scene file.
+	 * The files should have .json extensions and follow a specific format.
+	 * The file should be an array containing a list of objects with the following fields.<br>
+	 * "velocity" : [x,y,z]<br>
+	 * "mass"     : number larger than 0<br>
+	 * "color"    : [r,g,b]<br>
+	 * "shape"    : A JSONObject which can follow several formats:<br>
+	 * <br>
+	 * For a Sphere,<br>
+	 * "type"     : "Sphere"<br>
+	 * "location" : [x,y,z]<br>
+	 * "radius"   : number larger than 0<br>
+	 * <br>
+	 * For a RectangularPrism,<br>
+	 * this isn't implemented yet...<br>
+	 * <br>
+	 * Basic Example:<br>
+	 * <span style="text-indent: 0px;">[</span><br>
+	 * <span style="text-indent: 5px;">		{</span><br>
+	 * <span style="text-indent:10px;">			"velocity" : [0,0,50],</span><br>
+	 * <span style="text-indent:10px;">			"mass"     : 1,</span><br>
+	 * <span style="text-indent:10px;">			"color"    : [255,255,0],</span><br>
+	 * <span style="text-indent:10px;">			"shape"    : {</span><br>
+	 * <span style="text-indent:15px;">							"type" : "Sphere",</span><br>
+	 * <span style="text-indent:15px;">							"location" : [0, 0, 1000],</span><br>
+	 * <span style="text-indent:15px;">							"radius"   : 100</span><br>
+	 * <span style="text-indent:10px;">						}</span><br>
+	 * <span style="text-indent: 5px;">		}</span><br>
+	 * <span style="text-indent: 0px;">	]</span><br>
+	 */
+	public static void openSceneFile(){
+		//File chooser dialog
 		JFileChooser window = new JFileChooser();
 		window.showOpenDialog(window);
 		input = window.getSelectedFile();
+		
+		//Create objects from file
 		JSONArray set = new JSONArray(ParseFile.parseFile(input.getPath()));
 		for(int i = 0; i<set.length();i++){
 			System.out.println(0);
 			new PhysicalObject(set.getJSONObject(i));
 		}
-		
-		//
+	}
+	
+	public static void main(String[] args) {
+		renderList = new ArrayList<PhysicalObject>();
 		camera = new Camera(Vector.createFromRectangular(0, 0, 0), Vector.createFromRectangular(800, 600, 600), Vector.createFromRectangular(0, 0, 0));
-		//
 		
+		openSceneFile();
 		
 		//Builds the screen
 		try{
@@ -55,7 +87,6 @@ public class Start {
 			e.printStackTrace();
 			System.exit(0);
 		}
-		
 		
 		//I have no idea what this does
 		GL11.glMatrixMode(GL11.GL_PROJECTION);
@@ -68,6 +99,7 @@ public class Start {
 	}
 	
 	public static void loop() {
+		//Loop until the exit button is pressed
 		while (!Display.isCloseRequested()) {
 			//Move
 			for(PhysicalObject t_1: renderList){
@@ -85,7 +117,8 @@ public class Start {
 			//Update display
             Display.update();
         }
-         
+        
+		//Exit the window
         Display.destroy();
 	}
 }
