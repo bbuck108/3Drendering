9441af0d7d650fb01140328bfabe3b0b7b871827
Connor Lehmacher <clehmac@hotmail.com>
2016-05-04 10:36:04 -0400

    Fix big Problems

--- src/texturing/Texture.java
+++ src/texturing/Texture.java
@@ -6,28 +6,17 @@ import java.util.ArrayList;
 /** Texture for a physical object
  * @author Connor Lehmacher*/
 public class Texture {
-	private ArrayList<SubTexture> subTextures = new ArrayList<SubTexture>();
+	private ArrayList<Image> subTextures = new ArrayList<Image>();
 	
 	public Texture(String key) {
 		if(key.equals("Sphere")) {
-			//Top
-			subTextures.add(new SubTexture(null, new Link(1), new Link(1), new Link(1), new Link(1), 0));
-			//Bottom
-			subTextures.add(new SubTexture(null, new Link(0), new Link(0), new Link(0), new Link(0), 1));
+			//Full
+			subTextures.add(null);
 		}
 		if(key.equals("RectangularPrism")) {
 			// + X
-			subTextures.add(new SubTexture(null, new Link(1), new Link(2), new Link(4), new Link(5), 0));
-			// + Y
-			subTextures.add(new SubTexture(null, new Link(), new Link(), new Link(), new Link(), 1));
-			// + Z
-			subTextures.add(new SubTexture(null, new Link(), new Link(), new Link(), new Link(), 2));
-			// - X
-			subTextures.add(new SubTexture(null, new Link(), new Link(), new Link(), new Link(), 3));
-			// - Y
-			subTextures.add(new SubTexture(null, new Link(), new Link(), new Link(), new Link(), 4));
-			// - Z
-			subTextures.add(new SubTexture(null, new Link(), new Link(), new Link(), new Link(), 5));
+			subTextures.add(null);
+
 		}
 		else {
 			System.err.println("Unsupported Key");
@@ -35,8 +24,6 @@ public class Texture {
 	}
 	
 	public void defineImage(Image i, int id) {
-		SubTexture newSubTexture = subTextures.get(id);
-		newSubTexture.image = i;
-		subTextures.set(id, newSubTexture);
+
 	}
 }
